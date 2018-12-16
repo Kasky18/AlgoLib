a22d5b06e06555969058b4d59c2fe1a406e8aede
Martin Brazina <martin.brazina@code2b.com>
2018-12-16 23:21:07 +0100

    adding new numbers

--- Kotlin/InsertionSort.kt
+++ Kotlin/InsertionSort.kt
@@ -1,3 +1,5 @@
+import kotlin.system.measureTimeMillis
+
 fun insertionSort(array: IntArray) {
     for ((index) in array.withIndex()) {
         var j = index
@@ -18,8 +20,16 @@ fun insertionSort(array: IntArray) {
 }
 
 fun main(args: Array<String>) {
-    val array = intArrayOf(4, 3, 5, 6, 2, 1)
+
+    val array = intArrayOf(4, 3, 5, 9, 6, 7, 11, 2, 1, 15, 8, 9, 27, 32, 30, 55, 42, 40, 75, 82, 70, 120, 69, 77, 22,
+            100, 102, 94, 99, 65, 54, 85, 18, 62, 35, 111, 180, 165, 68, 44, 73, 49, 104, 44, 63, 200, 91, 190, 126, 50)
     array.forEach { i -> println(i) }
-    insertionSort(array)
+    val executionTime = measureTimeMillis {
+
+        insertionSort(array)
+
+    }
     array.forEach { i -> println(i) }
+    println("Execution Time = $executionTime ms")
+
 }
