09c4ea4406c4998ba714e102195e2151550bdf74
Martin Brazina <martin.brazina@code2b.com>
2018-12-16 23:54:47 +0100

    refactor

--- src/main/Kotlin/InsertionSort.kt
+++ src/main/Kotlin/InsertionSort.kt
@@ -1,4 +1,4 @@
-import kotlin.system.measureTimeMillis
+import kotlin.system.measureNanoTime
 
 fun insertionSort(array: IntArray) {
     for ((index) in array.withIndex()) {
@@ -24,7 +24,7 @@ fun main(args: Array<String>) {
     val array = intArrayOf(4, 3, 5, 9, 6, 7, 11, 2, 1, 15, 8, 9, 27, 32, 30, 55, 42, 40, 75, 82, 70, 120, 69, 77, 22,
             100, 102, 94, 99, 65, 54, 85, 18, 62, 35, 111, 180, 165, 68, 44, 73, 49, 104, 44, 63, 200, 91, 190, 126, 50)
     array.forEach { i -> println(i) }
-    val executionTime = measureTimeMillis {
+    val executionTime = measureNanoTime {
 
         insertionSort(array)
 
